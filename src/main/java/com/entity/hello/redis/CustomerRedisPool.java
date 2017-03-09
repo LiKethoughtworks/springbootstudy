e5b3e643735d0d9a5f73a9bb7f3423c76c06a245
likethoughtworks <kli@thoughtworks.com>
2017-03-09 11:03:00 +0800

    [like] Add pmd check

--- src/main/java/com/entity/hello/redis/CustomerRedisPool.java
+++ src/main/java/com/entity/hello/redis/CustomerRedisPool.java
@@ -3,13 +3,13 @@ package com.entity.hello.redis;
 import com.entity.hello.repository.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerRedisPool {
     public static final String CUSTMER_KEY = "mycustomer";
     public static final long OBJECT_KEY = 999L;
+
     @Autowired
     private RedisTemplate<String, Customer> redisTemplate;
 
